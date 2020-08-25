99ede08a248c96ab64c9c7068ce43f3c1e1175b3
antonshell <antonshell@yandex.ru>
2020-08-25 22:43:57 +0300

    Implemented load wallets

--- src/Command/LoadSampleData.php
+++ src/Command/LoadSampleData.php
@@ -3,6 +3,8 @@
 namespace App\Command;
 
 use App\Entity\User;
+use App\Entity\Wallet;
+use App\Enum\CurrencyEnum;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -58,6 +60,29 @@ class LoadSampleData extends Command
         $user3->setEmail('user3@test.com');
         $user3->setPassword(password_hash('user3', PASSWORD_DEFAULT));
         $this->entityManager->persist($user3);
+
+        $output->writeln('Create wallets...');
+
+        $wallet1 = new Wallet();
+        $wallet1->setUser($user1);
+        $wallet1->setName('BTC 1');
+        $wallet1->setBalance(10);
+        $wallet1->setCurrency(CurrencyEnum::BTC);
+        $this->entityManager->persist($wallet1);
+
+        $wallet2 = new Wallet();
+        $wallet2->setUser($user1);
+        $wallet2->setName('ETH 1');
+        $wallet2->setBalance(2);
+        $wallet2->setCurrency(CurrencyEnum::ETH);
+        $this->entityManager->persist($wallet2);
+
+        $wallet3 = new Wallet();
+        $wallet3->setUser($user2);
+        $wallet3->setName('BTC 2');
+        $wallet3->setBalance(3);
+        $wallet3->setCurrency(CurrencyEnum::BTC);
+        $this->entityManager->persist($wallet3);
 
         $this->entityManager->flush();
 
---
+++ src/Enum/CurrencyEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enum;
+
+class CurrencyEnum
+{
+    public const BTC = 'BTC';
+    public const ETH = 'ETH';
+}
