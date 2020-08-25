a931acc770ccb0d96beb70c88ee8eca951253502
antonshell <antonshell@yandex.ru>
2020-08-26 02:32:22 +0300

    Installed doctrine fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
